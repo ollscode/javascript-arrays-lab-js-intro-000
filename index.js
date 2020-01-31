c18d2b761c49cb85892665bbfe9ddb88f378cfc0
Oscar <mundonatural@gmail.com>
2020-01-31 08:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
 function destructivelyAppendKitten(item) {
   kittens.push(item);
+}
+
+function destructivelyAppendKitten(item) {
+  kittens.unshift(item);
 }
