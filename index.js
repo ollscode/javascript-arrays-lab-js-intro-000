266d4040c3911c4cfc167a8ed60511a9846e21c6
Oscar <mundonatural@gmail.com>
2020-01-31 08:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,5 +28,9 @@ function prependKitten(item) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
